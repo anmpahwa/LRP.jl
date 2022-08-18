4a1bf67712bebb202e91bc3b83ccdd35f997a879
anmol1104 <anmpahwa@ucdavis.edu>
2022-08-18 22:12:19 +0530

    update example

--- src/example.jl
+++ src/example.jl
@@ -2,11 +2,10 @@ using LRP
 using Revise
 using Random
 
-# Traveling Salesman Problem
-let 
-# Developing an optimal Traveling Salesman Problem solution
+let
+# Developing an optimal solution 
     # Define instance
-    instance = "att48"
+    instance = "rand200"
     # Define a random number generator
     rng = MersenneTwister(1234)
     # Build instance as graph
@@ -29,79 +28,13 @@ let
                     :randomnode!    , 
                     :relatedpair!   ,
                     :relatednode!   , 
-                    :worstnode!     ,  
-                ]                       , 
-        Ψᵢ  =   [
-                    :best!          ,
-                    :greedy!        ,
-                    :regret₂insert! ,
-                    :regret₃insert!
-                ]                       ,
-        Ψₗ  =   [
-                    :move!          ,
-                    :intraopt!      ,
-                    :split!         ,
-                    :swap!
-                ]                       ,
-        σ₁  =   33                      ,
-        σ₂  =   9                       ,
-        σ₃  =   13                      ,
-        ω   =   0.05                    ,
-        τ   =   0.5                     ,
-        𝜃   =   0.99975                 ,
-        C̲   =   30                      ,
-        C̅   =   60                      ,
-        μ̲   =   0.1                     ,
-        μ̅   =   0.4                     ,
-        ρ   =   0.1
-    )
-    # Run ALNS and fetch best solution
-    S = ALNS(rng, χ, sₒ)
-    s⃰ = S[end]      
-# Fetch objective function values
-    println("Initial: $(f(sₒ))")
-    println("Optimal: $(f(s⃰))")
-# Visualizations
-    # Visualize initial solution
-    display(visualize(sₒ)) 
-    # Visualize best solution
-    display(visualize(s⃰))
-    # Animate ALNS solution search process from inital to best solution
-    display(animate(S))
-    # Show convergence plots
-    display(convergence(S))
-    return
-end
-
-# Vehicle Routing Problem
-let
-# Developing an optimal Vehicle Routing Problem solution 
-    # Define instance
-    instance = "m-n101-k10"
-    # Define a random number generator
-    rng = MersenneTwister(1234)
-    # Build instance as graph
-    G = build(instance)
-    D, C, A = G
-    # Visualize instance
-    display(visualize(Solution(G...)))
-    # Define inital solution method and build the initial solution
-    method = :random
-    sₒ = initialsolution(rng, G, method)
-    # Define ALNS parameters
-    x = length(C) + length(D)
-    n = ceil(x, digits=-(length(digits(x))-1))
-    χ   = ALNSParameters(
-        k̲   =   n ÷ 25                  ,
-        l̲   =   2n                      ,
-        l̅   =   5n                      ,
-        k̅   =   10n                     ,
-        Ψᵣ  =   [
-                    :randomnode!    , 
-                    :relatedpair!   ,
-                    :relatednode!   , 
-                    :worstnode!     ,  
-                    :relatedroute!  
+                    :worstnode!     ,
+                    :randomroute!   ,  
+                    :relatedroute!  ,
+                    :worstroute!    ,
+                    :randomvehicle! ,
+                    :randomdepot!   ,
+                    :relateddepot!
                 ]                       , 
         Ψᵢ  =   [
                     :best!          ,
