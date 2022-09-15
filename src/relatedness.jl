3bf6c648bfe67eef6a000d201bc82a6333874912
anmol1104 <anmpahwa@ucdavis.edu>
2022-09-15 17:30:49 +0530

    add temporal dimension to relatedness

--- src/relatedness.jl
+++ src/relatedness.jl
@@ -1,20 +1,23 @@
 # Relatedness
 function relatedness(d¹::DepotNode, d²::DepotNode, a::Arc)
     l = a.l
+    t = 0
     q = abs(d¹.q - d².q)
     ϕ = false
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
 function relatedness(c¹::CustomerNode, c²::CustomerNode, a::Arc)
     l = a.l
+    t = abs(c¹.tᵉ - c².tᵉ) + abs(c¹.tˡ - c².tˡ)
     q = abs(c¹.q - c².q)
     ϕ = !isequal(c¹, c²) & isequal(c¹.r, c².r)
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
 function relatedness(c::CustomerNode, d::DepotNode, a::Arc)
     l = a.l
+    t = 0
     q = 0
     ϕ = false
     r = c.r
@@ -22,11 +25,12 @@ function relatedness(c::CustomerNode, d::DepotNode, a::Arc)
         ϕ = isequal(r.iᵛ, v.iᵛ) 
         ϕ ? break : continue
     end
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
 function relatedness(d::DepotNode, c::CustomerNode, a::Arc)
     l = a.l
+    t = 0
     q = 0
     ϕ = false
     r = c.r
@@ -34,36 +38,42 @@ function relatedness(d::DepotNode, c::CustomerNode, a::Arc)
         ϕ = isequal(r.iᵛ, v.iᵛ) 
         ϕ ? break : continue
     end
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
 function relatedness(r¹::Route, r²::Route)
     if !isopt(r¹) || !isopt(r²) return -Inf end
     if isequal(r¹, r²) return Inf end
     l = abs(r¹.l - r².l)
+    t = abs(r¹.tˢ - r².tˢ) + abs(r¹.tᵉ - r².tᵉ)
     q = abs(r¹.q - r².q)
     ϕ = isequal(r¹.iᵛ, r².iᵛ)
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
 function relatedness(v¹::Vehicle, v²::Vehicle)
     if !isopt(v¹) || !isopt(v²) return -Inf end
     if isequal(v¹, v²) return Inf end
     l¹ = 0.
+    t¹ = 0.
     q¹ = 0.
     for r ∈ v¹.R 
         l¹ += r.l
+        t¹ += r.l/v¹.s
         q¹ += r.q
     end
     l² = 0.
+    t² = 0.
     q² = 0.
     for r ∈ v².R
         l² += r.l
+        t² += r.l/v².s
         q² += r.q
     end
     l = abs(l¹ - l²)
+    t = abs(t¹ - t²)
     q = abs(q¹ - q²)
     ϕ = isequal(v¹.iᵈ, v².iᵈ)
-    z = 1/(l - q - ϕ)
+    z = 1/(l + t - q - ϕ)
     return z
 end
