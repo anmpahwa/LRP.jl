75f8e535f5e4f72c0a7366d2d273d0257d0715d3
anmol1104 <anmpahwa@ucdavis.edu>
2022-09-16 12:01:40 +0530

    add rand100 instance

--- src/example.jl
+++ src/example.jl
@@ -5,7 +5,7 @@ using Random
 let
 # Developing an optimal solution 
     # Define instance
-    instance = "rc201";
+    instance = "rand100";
     # Visualize instance
     display(visualize(instance))
     # Define a random number generator
