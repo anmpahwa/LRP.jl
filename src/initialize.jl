a09ea904d0992af96a7bfeb17d6d099aea8a7466
anmol1104 <anmpahwa@ucdavis.edu>
2023-11-25 02:28:39 -0800

    iterated clustering intialization

--- src/initialize.jl
+++ src/initialize.jl
@@ -1,62 +1,151 @@
 """
-    initialsolution([rng], G, method)
-
-Returns initial LRP `Solution` created using the given `method` for graph `G` given as a tuple of nodes and arcs.
-Available methods include,
-- K-means Clustering Initialization : `:cluster`
-- Random Initialization             : `:random`
-
-Optionally specify a random number generator `rng` as the first argument
-(defaults to `Random.GLOBAL_RNG`).
+    build(instance::String)
+    
+Returns a tuple of depot nodes, customer nodes, and arcs for the `instance`.
 """
-initialsolution(rng::AbstractRNG, G, method::Symbol)::Solution = isdefined(LRP, method) ? getfield(LRP, method)(rng, G) : getfield(Main, method)(rng, G)
-initialsolution(G, method::Symbol) = initialsolution(Random.GLOBAL_RNG, G, method)
+function build(instance::String)
+    # Depot nodes
+    df = DataFrame(CSV.File(joinpath(dirname(@__DIR__), "instances/$instance/depot_nodes.csv")))
+    D  = Vector{DepotNode}(undef, nrow(df))
+    for k ∈ 1:nrow(df)
+        iⁿ = df[k,1]
+        jⁿ = df[k,2]
+        x  = df[k,3]
+        y  = df[k,4]
+        qᵈ = df[k,5]
+        tˢ = df[k,6]
+        tᵉ = df[k,7]
+        τ  = Inf
+        n  = 0
+        q  = 0.
+        l  = 0.
+        πᵒ = df[k,8]
+        πᶠ = df[k,9]
+        φ  = df[k,10]
+        d  = DepotNode(iⁿ, jⁿ, x, y, qᵈ, tˢ, tᵉ, τ, n, q, l, πᵒ, πᶠ, φ, Vehicle[])
+        D[iⁿ] = d
+    end
+
+    # Customer nodes
+    df = DataFrame(CSV.File(joinpath(dirname(@__DIR__), "instances/$instance/customer_nodes.csv")))
+    I  = (df[1,1]:df[nrow(df),1])
+    C  = OffsetVector{CustomerNode}(undef, I)
+    for k ∈ 1:nrow(df)
+        iⁿ = df[k,1]
+        iʳ = 0
+        iᵛ = 0
+        iᵈ = 0
+        x  = df[k,2]
+        y  = df[k,3]
+        q  = df[k,4]
+        τᶜ = df[k,5]
+        tᵉ = df[k,6]
+        tˡ = df[k,7]
+        iᵗ = 0
+        iʰ = 0
+        tᵃ = 0.
+        tᵈ = 0.
+        c  = CustomerNode(iⁿ, iʳ, iᵛ, iᵈ, x, y, q, τᶜ, tᵉ, tˡ, iᵗ, iʰ, tᵃ, tᵈ, NullRoute)
+        C[iⁿ] = c
+    end
+
+    # Arcs
+    df = DataFrame(CSV.File(joinpath(dirname(@__DIR__), "instances/$instance/arcs.csv"), header=false))
+    A  = Dict{Tuple{Int64,Int64},Arc}()
+    N  = length(D)+length(C)
+    for iᵗ ∈ 1:N
+        for iʰ ∈ 1:N
+            l = df[iᵗ,iʰ] 
+            a = Arc(iᵗ, iʰ, l)
+            A[(iᵗ,iʰ)] = a
+        end
+    end
+
+    # Vehicles
+    df = DataFrame(CSV.File(joinpath(dirname(@__DIR__), "instances/$instance/vehicles.csv")))
+    for k ∈ 1:nrow(df)
+        d  = D[df[k,3]]
+        iᵛ = df[k,1]
+        jᵛ = df[k,2]
+        iᵈ = df[k,3]
+        qᵛ = df[k,4]
+        lᵛ = df[k,5]
+        sᵛ = df[k,6]
+        τᶠ = df[k,7]
+        τᵈ = df[k,8]
+        τᶜ = df[k,9]
+        τʷ = df[k,10]
+        r̅  = df[k,11]
+        tˢ = d.tˢ
+        tᵉ = d.tˢ
+        τ  = Inf
+        n  = 0
+        q  = 0.
+        l  = 0.
+        πᵈ = df[k,12]
+        πᵗ = df[k,13]
+        πᶠ = df[k,14]
+        v  = Vehicle(iᵛ, jᵛ, iᵈ, qᵛ, lᵛ, sᵛ, τᶠ, τᵈ, τᶜ, τʷ, r̅, tˢ, tᵉ, τ, n, q, l, πᵈ, πᵗ, πᶠ, Route[])
+        push!(d.V, v)
+    end
+    V  = [v for d ∈ D for v ∈ d.V]
+
+    φᵈ = iszero(getproperty.(D, :tˢ)) && iszero(getproperty.(D, :tᵉ))
+    φᶜ = iszero(getproperty.(C, :tᵉ)) && iszero(getproperty.(C, :tˡ))
+    φᵛ = iszero(getproperty.(V, :τʷ)) && iszero(getproperty.(V, :πᵗ))
+    global φᵀ = !(φᵈ && φᶜ && φᵛ)::Bool
+    
+    G  = (D, C, A)
+    return G
+end
 
 
 
 """
-    cluster(rng::AbstractRNG, G)
+    cluster(rng::AbstractRNG, k::Int64, instance::String)
 
-Returns initial `Solution` created using k-means clustering algorithm.
+Returns `Solution` created using `k`-means clustering algorithm.
+Here, each cluster is assigned to the nearest depot node and 
+then each customer in this cluster is greedy inserted to the 
+assigned depot node.
 """
-function cluster(rng::AbstractRNG, G)
+function cluster(rng::AbstractRNG, k::Int64, instance::String)
+    # Step 1: Initialize
+    G = build(instance)
     s = Solution(G...)
     preinitialize!(s)
     D = s.D
     C = s.C
-    V = [v for d ∈ D for v ∈ d.V]
-    # Step 1: Initialize
-    W = zeros(4, eachindex(C))
-    for (iⁿ,c) ∈ pairs(C) W[:,iⁿ] = [c.x, c.y, c.tᵉ, c.tˡ] end
-    # Step 2: Cluster customer nodes
-    K = kmeans(W.parent, min(length(C),length(V)); rng=rng)
+    # Step 2: Clustering
+    N = zeros(4, eachindex(C))
+    for (iⁿ,c) ∈ pairs(C) N[:,iⁿ] = [c.x, c.y, c.tᵉ, c.tˡ] end
+    K = kmeans(N.parent, k; rng=rng)
     A = OffsetVector(K.assignments, eachindex(C))
-    Cᵒ = K.centers
-    Iᵒ = 1:size(Cᵒ)[2]
-    # Step 3: Build initial solution by assigning each cluster to the closest available depot node and consequently inserting customer nodes into the routes
-    for iᵒ ∈ Iᵒ
-        Z = fill(Inf, length(D))
-        for (iⁿ,d) ∈ pairs(D)
-            if all(isopt, d.V) continue end
-            xᵒ = Cᵒ[1,iᵒ]
-            yᵒ = Cᵒ[2,iᵒ]
+    M = K.centers
+    # Step 3: Add customers from each cluster to the assigned depot
+    for k ∈ 1:nclusters(K)
+        Y = fill(Inf, length(D))  
+        for j ∈ eachindex(Y)
+            d  = D[j]
+            xᵒ = M[1,k]
+            yᵒ = M[2,k]
             xᵈ = d.x
             yᵈ = d.y
-            Z[iⁿ] = sqrt((xᵒ-xᵈ)^2 + (yᵒ-yᵈ)^2)
+            Y[j] = sqrt((xᵒ-xᵈ)^2 + (yᵒ-yᵈ)^2)
         end
-        iⁿ= argmin(Z)
-        d = D[iⁿ]
+        d = D[argmin(Y)]
         R = [r for v ∈ d.V for r ∈ v.R]
-        L = filter(c -> isequal(A[c.iⁿ], iᵒ), C)
+        L = filter(c -> isequal(A[c.iⁿ], k), C)
+        if isempty(L) continue end
         I = eachindex(L)
         J = eachindex(R)
         X = ElasticMatrix(fill(Inf, (I,J)))     # X[i,j]: insertion cost of customer node L[i] at best position in route R[j]
         P = ElasticMatrix(fill((0, 0), (I,J)))  # P[i,j]: best insertion postion of customer node L[i] in route R[j]
-        ϕ = ones(Int64, J)                      # ϕ[j]  : selection weight for route R[j]
-        # Step 3.1: Iterate until all customer nodes of the cluster have been inserted into the route
+        ϕ = ones(Int64, J)                      # ϕ[j]  : binary weight for route R[j]
+        # Step 3.1: Iterate until all open customer nodes have been inserted into the route
         for _ ∈ I
-            # Step 3.1.1: Iterate through all customer nodes of the cluster and every possible insertion position in each route
-            zᵒ = f(s)
+            # Step 3.1.1: Iterate through all open customer nodes and every possible insertion position in each route
+            z = f(s)
             for (i,c) ∈ pairs(L)
                 if !isopen(c) continue end
                 for (j,r) ∈ pairs(R)
@@ -70,8 +159,8 @@ function cluster(rng::AbstractRNG, G)
                         # Step 3.1.1.1: Insert customer node c between tail node nᵗ and head node nʰ in route r
                         insertnode!(c, nᵗ, nʰ, r, s)
                         # Step 3.1.1.2: Compute the insertion cost
-                        z⁺ = f(s)
-                        Δ  = z⁺ - zᵒ
+                        z′ = f(s)
+                        Δ  = z′ - z
                         # Step 3.1.1.3: Revise least insertion cost in route r and the corresponding best insertion position in route r
                         if Δ < X[i,j] X[i,j], P[i,j] = Δ, (nᵗ.iⁿ, nʰ.iⁿ) end
                         # Step 3.1.1.4: Remove customer node c from its position between tail node nᵗ and head node nʰ
@@ -82,12 +171,13 @@ function cluster(rng::AbstractRNG, G)
                     end
                 end
             end
-            # Step 3.1.2: Insert customer node with least insertion cost at its best position
-            i,j = Tuple(argmin(X))
-            c = L[i]
-            r = R[j]
-            d = s.D[r.iᵈ]
-            v = d.V[r.iᵛ]
+            # Step 3.1.2: Randomly select a customer node to insert at its best position
+            i,j= Tuple(argmin(X))
+            Δ  = X[i,j]
+            c  = L[i]
+            r  = R[j]
+            d  = s.D[r.iᵈ]
+            v  = d.V[r.iᵛ]
             iᵗ = P[i,j][1]
             iʰ = P[i,j][2]
             nᵗ = iᵗ ≤ length(D) ? D[iᵗ] : C[iᵗ]
@@ -98,12 +188,16 @@ function cluster(rng::AbstractRNG, G)
             P[i,:] .= ((0, 0), )
             ϕ .= 0
             for (j,r) ∈ pairs(R) 
-                if !isequal(r.iᵛ, v.iᵛ) continue end
+                φʳ = isequal(r, c.r)
+                φᵛ = isequal(r.iᵛ, v.iᵛ) && isless(c.r.tⁱ, r.tⁱ) && isequal(φᵀ, true)
+                φᵈ = isequal(r.iᵈ, d.iⁿ) && !hasslack(d)
+                φˢ = φʳ || φᵛ || φᵈ
+                if isequal(φˢ, false) continue end
                 X[:,j] .= Inf
                 P[:,j] .= ((0, 0), )
-                ϕ[j] = 1  
+                ϕ[j] = 1
             end
-            # Step 3.1.4: Update solution appropriately 
+            # Step 3.1.4: Update solution appropriately     
             if addroute(r, s)
                 r = Route(v, d)
                 push!(v.R, r)
@@ -130,32 +224,40 @@ function cluster(rng::AbstractRNG, G)
 end
 
 
-
 """
-    random(rng::AbstractRNG, G)
+    initialize(rng::AbstractRNG, instance::String)
 
-Returns initial `Solution` created with randomly selcted node-route combination until all customer nodes have been added to the solution.
+Returns initial LRP `Solution` developed using iterated clustering method. 
+The number of clusters are increased iteratively for atleast as many iterations 
+as the number of depot nodes and until a feasible solution is found or atmost 
+the number of customer nodes. Finally, the solution with the least objective 
+function value is returned as the initial solution.
 """
-function random(rng::AbstractRNG, G)
-    s = Solution(G...)
-    preinitialize!(s)
-    D = s.D
-    C = s.C
-    # Step 1: Initialize
-    d = sample(rng, D)
-    v = sample(rng, d.V)
-    r = sample(rng, v.R)
-    W = ones(Int64, eachindex(C))                      # W[i]: selection weight for customer node C[i]
-    # Step 2: Iteratively append randomly selected customer node in randomly selected route
-    while any(isopen, C)
-        c  = sample(rng, C, OffsetWeights(W))
-        nᵗ = d
-        nʰ = isopt(r) ? C[r.iˢ] : D[r.iˢ]
-        insertnode!(c, nᵗ, nʰ, r, s)
-        iⁿ = c.iⁿ
-        W[iⁿ] = 0
+function initialize(rng::AbstractRNG, instance::String)
+    # Step 1. Initialize
+    s = Solution(build(instance)...)
+    z = Inf
+    # Step 2. Iteratively increase the number of clusters
+    # for atleast as many iterations as the number of depot 
+    # nodes and until a feasible solution is found or atmost 
+    # the number of customer nodes.
+    k = 0
+    k̲ = length(s.D)
+    k̅ = length(s.C)
+    ϕ = false
+    while k < k̅
+        k += 1
+        s′ = cluster(rng, k, instance)
+        z′ = f(s′)
+        # Step 2.1. Update solution
+        if z′ < z
+            z = z′ 
+            s = deepcopy(s′)
+        end
+        # Step 2.2. Check for break conditions
+        ϕ = ϕ || isfeasible(s′)
+        k < k̲ ? continue : (ϕ ? break : continue)
     end
-    postinitialize!(s)
-    # Step 4: Return initial solution
+    # Step 3. Return solution
     return s
 end
