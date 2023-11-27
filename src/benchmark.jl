82dfa5e3c37df44b50e96b4e594acd6ad7b8cd92
anmol1104 <anmpahwa@ucdavis.edu>
2023-11-27 00:00:43 -0800

    update benchmark

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -64,7 +64,8 @@ let
                             :intraopt!          ,
                             :intermove!         ,
                             :interswap!         ,
-                            :interopt!          
+                            :interopt!          ,
+                            :swapdepot!
                         ]                       ,
                 σ₁  =   15                      ,
                 σ₂  =   10                      ,
