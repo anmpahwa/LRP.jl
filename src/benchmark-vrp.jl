90f45b7c5e1e6a0f77f610e733503e8122fd0077
anmol1104 <anmpahwa@ucdavis.edu>
2023-12-14 17:50:48 -0800

    update benchmarks

--- src/benchmark-vrp.jl
+++ src/benchmark-vrp.jl
@@ -12,7 +12,7 @@ let
     B = ["r101", "r201", "c101", "c201", "rc101", "rc201"]
     instances = [A..., B...,]
     # Define random number generators
-    seeds = [1010, 1104, 1509, 1604, 1905, 2104, 2412, 2703, 2710, 2807]
+    seeds = [1010, 1106, 1509, 1604, 1905, 2104, 2412, 2703, 2710, 2807]
     # Dataframes to store solution quality and run time
     df₁ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ 0:length(seeds)])
     df₂ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ 0:length(seeds)])
