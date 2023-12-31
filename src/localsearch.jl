d529c3ba9a04783992933ee260e5834ab964103f
anmol1104 <anmpahwa@ucdavis.edu>
2023-12-30 21:32:24 -0800

    update m

--- src/localsearch.jl
+++ src/localsearch.jl
@@ -98,7 +98,7 @@ function intermove!(rng::AbstractRNG, k̅::Int, s::Solution)
         r₁ = c.r
         if isdormant(r₁) continue end
         # Step 2.2: Select a random route
-        m  = sample(rng, s.φ ? [:x, :q, :l, :t] : [:x, :q, :l])
+        m  = sample(rng, s.φ ? [:q, :l, :t] : [:q, :l])
         Wʳ = [isdormant(r₂) || isequal(r₁, r₂) ? 0. : relatedness(m, r₁, r₂, s) for r₂ ∈ R]
         r₂ = sample(rng, R, Weights(Wʳ))
         if isdormant(r₂) continue end
@@ -163,7 +163,7 @@ function intraswap!(rng::AbstractRNG, k̅::Int, s::Solution)
         # n₁ → n₂ → n₃ and n₄ → n₅ → n₆
         n₂ = sample(rng, C, OffsetWeights(W₂))
         if isdormant(n₂) continue end
-        m  = sample(rng, s.φ ? [:x, :q, :l, :t] : [:x, :q, :l])
+        m  = sample(rng, s.φ ? [:q, :l, :t] : [:q, :l])
         W₅ = [isdormant(n₅) || !isequal(n₂.r, n₅.r) || isequal(n₂, n₅) ? 0. : relatedness(m, n₂, n₅, s) for n₅ ∈ C]
         n₅ = sample(rng, C, OffsetWeights(W₅))
         if isdormant(n₅) continue end
@@ -237,7 +237,7 @@ function interswap!(rng::AbstractRNG, k̅::Int, s::Solution)
         # n₁ → n₂ → n₃ and n₄ → n₅ → n₆
         n₂ = sample(rng, C, OffsetWeights(W₂))
         if isdormant(n₂) continue end
-        m  = sample(rng, s.φ ? [:x, :q, :l, :t] : [:x, :q, :l])
+        m  = sample(rng, s.φ ? [:q, :l, :t] : [:q, :l])
         W₅ = [isdormant(n₅) || isequal(n₂.r, n₅.r) || isequal(n₂, n₅) ? 0. : relatedness(m, n₂, n₅, s) for n₅ ∈ C]
         n₅ = sample(rng, C, OffsetWeights(W₅))
         if isdormant(n₅) continue end
@@ -398,7 +398,7 @@ function interopt!(rng::AbstractRNG, k̅::Int, s::Solution)
         # d₂ → ... → n₁ → n₂ → n₃ → ... → d₂ and d₅ → ... → n₄ → n₅ → n₆ → ... → d₅
         r₂ = sample(rng, R, Weights(W₂))
         if !isopt(r₂) || isdormant(r₂) continue end
-        m  = sample(rng, s.φ ? [:x, :q, :l, :t] : [:x, :q, :l])
+        m  = sample(rng, s.φ ? [:q, :l, :t] : [:q, :l])
         W₅ = [!isopt(r₅) || isdormant(r₅) || isequal(r₂, r₅) ? 0. : relatedness(m, r₂, r₅, s) for r₅ ∈ R]
         r₅ = sample(rng, R, Weights(W₅))
         if !isopt(r₅) || isdormant(r₅) continue end
@@ -520,7 +520,7 @@ function swapdepot!(rng::AbstractRNG, k̅::Int, s::Solution)
         d₁ = sample(rng, D, Weights(W₁))
         R₁ = [r₁ for v₁ ∈ d₁.V for r₁ ∈ v₁.R]
         if any(isdormant, R₁) continue end
-        m  = sample(rng, s.φ ? [:x, :q, :l, :t] : [:x, :q, :l])
+        m  = sample(rng, s.φ ? [:q, :l, :t] : [:q, :l])
         W₂ = [isequal(d₁, d₂) ? 0. : relatedness(m, d₁, d₂, s) for d₂ ∈ D]
         d₂ = sample(rng, D, Weights(W₂))
         R₂ = [r₂ for v₂ ∈ d₂.V for r₂ ∈ v₂.R]
